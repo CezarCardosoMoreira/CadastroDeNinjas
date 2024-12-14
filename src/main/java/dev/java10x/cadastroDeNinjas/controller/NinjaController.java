88523b41176d082b5949d7f41058327fff62e10f
cezar <cezargithub@gmail.com>
2024-12-13 21:54:59 -0300

    criação da entidade ninjamode para o banco de dadosl

--- src/main/java/dev/java10x/cadastroDeNinjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/controller/NinjaController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping()
-public class Controller {
+public class NinjaController {
 
 
-    @GetMapping("/ola")
+    @GetMapping("/oi")
     public String boasvindas(){
         return "Ola";
     }
